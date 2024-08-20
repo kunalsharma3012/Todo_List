6bf7b3e6687b4c098c3d14be8cfc83d03ffea8c1
kunalsharma3012 <kunalsharma93103@gmail.com>
2024-08-20 13:19:41 +0530

    last-commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Navbar() {
   return (
     
-    <nav className="flex justify-around bg-slate-900 text-white">
+    <nav className="flex justify-around bg-slate-800 text-white">
         <div className='font-bold text-xl py-2 mx-8'>YourTodos</div>
         <ul className='flex  justify-center items-center mx-9 gap-8  '>
             <li className='hover:font-bold cursor-pointer transition-all
